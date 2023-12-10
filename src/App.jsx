6c3134a8e6e482aa2a6c850f277ba5558840a4e9
Fran <faibarra1@uc.cl>
2023-12-10 00:49:12 -0300

    feat: base project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+
+const App = () => {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App;
