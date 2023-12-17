a079f5271ca804f5cf444d0dd26dfb6931b429a4
Fran <faibarra1@uc.cl>
2023-12-17 03:38:14 -0300

    feat: loading to desktop

--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -6,19 +6,62 @@ import { Mprserv120 } from '@react95/icons';
 import { Button, TextInput } from 'react95';
 
 const Container = styled.div`
-
+  display: flex;
+  height: 100%;
 `
+
+const LoginContainer = styled.div`
+  width: 500px;
+  margin: auto;
+`;
+
+const Row = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  justify-content: space-between;
+`;
+
+const Text = styled.span`
+  color: white;
+  font-size: 28px;
+`;
+
+const LoginIcon = styled(Mprserv120)``
+
+const Inputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
 
 const StartView = () => {
   const { computerState } = useContext(WindowContext);
 
   return (
     <Container>
-      oowoow
-      <Mprserv120 variant="48x48_4"/>
-      <TextInput></TextInput>
-      <TextInput></TextInput>
-      <Button></Button>
+      <LoginContainer>
+        <Row>
+          <LoginIcon variant="48x48_4"/>
+          <Inputs>
+            <Row>
+              <Text>Username</Text>
+              <TextInput 
+                value="gatochico"
+              />
+            </Row>
+            <Row>
+              <Text>Password</Text>
+              <TextInput 
+                value="password"
+                type="password"
+              />
+            </Row>
+          </Inputs>
+        <Button>ENTER</Button>
+        </Row>
+      </LoginContainer>
     </Container>
   )
 }
